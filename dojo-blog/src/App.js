61a51fb56bae9427fa5c0f4d37463cae12c77dd3
Shravani Sawant <sawantshravani03@gmail.com>
2023-04-07 00:35:52 +0530

    Inital Commit

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="div">
+        <h1>Starting with the ReactApp</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
