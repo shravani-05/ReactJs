0c3b4c6c4a4618ecfa0c5be72d9f761bff767b84
Shravani Sawant <sawantshravani03@gmail.com>
2023-05-26 01:15:03 +0530

    Onclick Events

--- dojo-blog/src/homePage.js
+++ dojo-blog/src/homePage.js
@@ -1,7 +1,25 @@
 const HomePage = () => {
+    const name = "shravani";
+    const handleClick = () =>{
+        window.alert("You just clicked the button " +  name);
+        console.log("You just clicked the button");
+    }
+    
+    const handleClickAgain = (name) => {
+        window.alert("You just clicked the button " +  name);
+
+    }
+    
+    
     return (  
-        <div className="content">
+           
+      <div className="content">
+        {/* passing an argument to string */}
+            <button onClick={handleClick}>Click me</button>
             
+            {/* passing an argument to the function */}
+            
+            <button onClick={() =>{window.alert("You just clicked the button " + name); }}>Click me again</button>
         </div>
     );
 }
