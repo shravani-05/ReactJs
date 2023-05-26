f370a5aee37b198629c34eca65db1bcfac6b9f68
Shravani Sawant <sawantshravani03@gmail.com>
2023-05-26 16:34:00 +0530

    Using Hooks(useState)

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -6,7 +6,11 @@ const Navbar = () => {
     <ul class="nav">
     <li><h1>My Own Blog</h1></li>  
  
-    <li ><a href="*">Home</a></li>
+    <li ><a href="*" style={{
+        backgroundColor: "#FF597B",
+        color: "white",
+        borderRadius: "5px"
+    }}>Home</a></li>
     <li><a href="*" style={{
         backgroundColor: "#FF597B",
         color: "white",
--- dojo-blog/src/homePage.js
+++ dojo-blog/src/homePage.js
@@ -1,26 +1,24 @@
+import { useState } from "react";
 const HomePage = () => {
-    const name = "shravani";
+    // const name = "shravani";
+    const [name,setName] = useState('Shravani');
+    const [age, setAge] = useState(20);
     const handleClick = () =>{
-        window.alert("You just clicked the button " +  name);
-        console.log("You just clicked the button");
+       setName('Om')
+       setAge('15')
     }
     
-    const handleClickAgain = (name) => {
-        window.alert("You just clicked the button " +  name);
-
-    }
+    
     
     
     return (  
            
       <div className="content">
+        <p>{name} is {age} years old. </p>
         {/* passing an argument to string */}
             <button onClick={handleClick}>Click me</button>
             
-            {/* passing an argument to the function */}
-            
-            <button onClick={() =>{window.alert("You just clicked the button " + name); }}>Click me again</button>
-        </div>
+            </div>
     );
 }
  
